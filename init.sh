e6873f78e2f008104a58cbeb220ea1a5027cac8c
patrickphat <me@patrickphat.com>
2021-08-01 14:54:31 +0700

    update init

--- init.sh
+++ init.sh
@@ -3,13 +3,17 @@
 printf "\n===== Copying config files.. ====="
 cp -a backup_dotfiles/.  ~/ 
 
-printf "\n===== Setup YouCompleteMe.. ====="
+printf "\n===== Install Vim Vundle ====="
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+printf "\n===== Install Vim Plugin ====="
 vim +PluginInstall +qall
+
+printf "\n==== Install coc.nvim plugins ===="
+vim -c ":CocInstall coc-pyright coc-tabnine coc-html" -c "q"
 
 printf "\n===== Install fzf.. ====="
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 bash ~/.fzf/install --all
-
 
 printf "\n==== DONE ===="
